cc92ac2d4173be8d18910795e7ef151044456689
Edissongg <edisson.garcia@unillanos.edu.co>
2024-12-18 11:41:16 -0500

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,7 @@
-# convertir/urls.py
 from django.urls import path
 from .views import ConvertirNumero
+
+app_name = 'projects'
 
 urlpatterns = [
     path('projects/', ConvertirNumero.as_view(), name='projects'),
